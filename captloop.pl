d778aa68d6b5fccc82aab07831f51e4463d9739a
marcwgit <marcwgit@gmail.com>
2016-04-04 15:29:37 -0400

    renumber 2

--- captloop.pl
+++ captloop.pl
@@ -81,29 +81,21 @@ system ("srttool", $arg11 ,$arg21 , $arg31  );
     $arg3 = "-o out.srt";
     $arg4 = 0;
    system ("srttool" , $arg1 , $arg2 , $arg3 );
-
     $filelastcap =  $basename . "3.srt";
     $arg5 = "combfile.srt";
     $arg6 = "out.srt";
     $arg7 = "";
-
     @CATS = ('cap_MonMar21_01_01PM-3.srt', 'out.srt');
-
       open SEL, '>', 'selected.srt' or die $!;
       FILE: foreach (@CATS) {
-
          open(FILE, $_) || ((warn "Can't open file $_\n"), next FILE);
-
          print "$_ \n";
-
          while (<FILE>) {
             print SEL;
          }
          close(FILE);
-
       }
       close(SEL);
-
 #third
        $timestring = "ffprobe -i " . $basename . "2.m4a   -v quiet -show_format | sed -n 's/duration=//p'";
        $time = `$timestring`;
@@ -114,31 +106,21 @@ system ("srttool", $arg11 ,$arg21 , $arg31  );
        $arg3 = "-o out.srt";
        $arg4 = 0;
       system ("srttool" , $arg1 , $arg2 , $arg3 );
-
        $filelastcap =  $basename . "2.srt";
        $arg5 = "combfile.srt";
        $arg6 = "out.srt";
        $arg7 = "";
-
        @CATS = ('cap_MonMar21_01_01PM-2.srt', 'out.srt');
-
         open SEL, '>', 'selected.srt' or die $!;
          FILE: foreach (@CATS) {
-
             open(FILE, $_) || ((warn "Can't open file $_\n"), next FILE);
-
             print "$_ \n";
-
             while (<FILE>) {
                print SEL;
             }
             close(FILE);
-
          }
          close(SEL);
-
-
-
 #fourth
           $timestring = "ffprobe -i " . $basename . "1.m4a   -v quiet -show_format | sed -n 's/duration=//p'";
           $time = `$timestring`;
@@ -149,26 +131,19 @@ system ("srttool", $arg11 ,$arg21 , $arg31  );
           $arg3 = "-o out.srt";
           $arg4 = 0;
          system ("srttool" , $arg1 , $arg2 , $arg3 );
-
           $filelastcap =  $basename . "4.srt";
           $arg5 = "combfile.srt";
           $arg6 = "out.srt";
           $arg7 = "";
-
           @CATS = ('cap_MonMar21_01_01PM-1.srt', 'out.srt');
-
             open SEL, '>', 'selected.srt' or die $!;
             FILE: foreach (@CATS) {
-
                open(FILE, $_) || ((warn "Can't open file $_\n"), next FILE);
-
                print "$_ \n";
-
                while (<FILE>) {
                   print SEL;
                }
                close(FILE);
-
             }
             close(SEL);
             =cut
